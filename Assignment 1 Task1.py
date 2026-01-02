d9cad696731a954c14b61569f7ed695ce8e7d9de
NABANITA BARAI <nabanitabarai08@gmail.com>
2026-01-02 22:02:49 +0530

    initial commit

---
+++ Assignment 1 Task1.py
@@ -0,0 +1,20 @@
+#Module 2 : Basic Python concept
+#Task 1 : Write a Python programme
+
+num1 = int(input("Enter the first number : "))
+num2 = int(input("Enter the second number : "))
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+print("Addition : ", addition)
+print("Subtraction : ", subtraction)
+print("Multiplication : ", multiplication)
+print("Division : ", division)
+
+
+
+
+
---
+++ Assignment 1 Task2.py
@@ -0,0 +1,9 @@
+#Module 2 : Basic Python concept
+#Task 2: Create a Personalized Greeting
+
+name_1 = input("Enter your first name : ")
+name_2 = input("Enter your last name : ")
+full_name = name_1 + " " + name_2
+print("Hello," + full_name + "! Welcome to the Python programme.")
+
+
